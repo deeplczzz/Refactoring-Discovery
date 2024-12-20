2ef0e2b62aaf27318d27fab9390a18221d4f7623
chen liu <edge225876@gmail.com>
2024-12-20 15:48:48 +0800

    remove legend of pie chart

--- entry/Pie-Chart/PieChart-Config.js
+++ entry/Pie-Chart/PieChart-Config.js
@@ -1,36 +1,33 @@
-export const PieConfig = (refactoringData) => ({
-    appendPadding: 10,
-    data: refactoringData,
-    angleField: 'value',
-    colorField: 'type',
-    radius: 0.7,
-    innerRadius: 0.6,
-    statistic: false,
-    width: 400,
-    height: 400,
-    //animation: false,
-    label: {
-        type: 'spider',
-        content: '{name}({value})',
-        style: {
-            fontSize: 12,
-            fill: 'black',
+export const PieConfig = (refactoringData) => {
+    return {
+        appendPadding: 10,
+        data: refactoringData,
+        angleField: 'value',
+        colorField: 'type',
+        radius: 0.7,
+        innerRadius: 0.6,
+        statistic: false,
+        // width: '100%',
+        // height: '100%',
+        //animation: false,
+        label: {
+            type: 'spider',
+            content: '{name}({value})',
+            style: {
+                fontSize: 12,
+                fill: 'black',
+            },
         },
-    },
-    legend: {
-        position: 'right',
-        item: {
-            onClick: null,
+        legend: false ,
+        interactions: [
+            { type: 'element-selected' },
+            { type: 'element-active' },
+            { type: 'legend-filter', enable: false },
+        ],
+        tooltip: {
+            formatter: (datum) => {
+                return { name: datum.type, value: datum.value };
+            },
         },
-    },
-    interactions: [
-        { type: 'element-selected' },
-        { type: 'element-active' },
-        { type: 'legend-filter', enable: false },
-    ],
-    tooltip: {
-        formatter: (datum) => {
-            return { name: datum.type, value: datum.value };
-        },
-    },
-});
+    };
+};
